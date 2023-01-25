f496ff575f910425064d0bdaa339d60f8d1b3828
GeezRamPy <123511546+GeezRamPy@users.noreply.github.com>
2023-01-26 02:53:53 +0700

    Update inline.py

--- rams/modules/bot/inline.py
+++ rams/modules/bot/inline.py
@@ -90,10 +90,12 @@ async def alive_function(message: Message, answers):
 async def ping_function(message: Message, answers):
     msg = (
         f"𝗥𝗮𝗺𝗣𝘆𝗿𝗼-𝗠𝗮𝘀𝘁𝗲𝗿 \n"
-        "ㅤㅤStatus : __Menyala!__ \n"
-        f"ㅤㅤㅤㅤmodules:</b> <code>{len(modules)} Modules</code> \n"
-        f"ㅤㅤㅤㅤbot version: {BOT_VER} \n"
-        f"ㅤㅤㅤㅤbranch: {branch} \n\n"
+        "ㅤㅤ⋙ sᴛᴀᴛᴜs : 𝗔𝗸𝘁𝗶𝗳!!! \n"
+        f"ㅤㅤㅤㅤ⋙ ᴍᴏᴅᴜʟᴇs: </b> <code>{len(modules)} Modules</code> \n"
+        f"ㅤㅤㅤㅤ⋙ ᴠᴇʀsɪ ʙᴏᴛ: {BOT_VER} \n"
+        f"ㅤㅤㅤㅤ⋙ ʙʀᴀɴᴄʜ: {branch} \n"
+        f"ㅤㅤㅤㅤ⋙ ᴠᴇʀsɪ ᴘʏʀᴏ: </b> <code>{pyrover}</code>\n"
+        f"ㅤㅤㅤㅤ⋙ ᴠᴇʀsɪ ᴘʏᴛʜᴏɴ: </b> <code>{pyver.split()[0]}</code>"
     )
     answers.append(
         InlineQueryResultArticle(
@@ -104,7 +106,7 @@ async def ping_function(message: Message, answers):
                 msg, parse_mode=ParseMode.HTML, disable_web_page_preview=True
             ),
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton(text="⇕ support ⇕", url="t.me/GeezRam"), InlineKeyboardButton(text="◇ owner ◇", url="t.me/thisrama")], [InlineKeyboardButton(text="❈ menu inline ❈", callback_data="reopen")]]
+                [[InlineKeyboardButton(text="❈ sᴜᴘᴘᴏʀᴛ ❈", url="t.me/GeezRam")], [InlineKeyboardButton(text="❈ ᴛᴜᴛᴜᴘ ❈", callback_data="close"), InlineKeyboardButton(text="❈ ʙᴜᴋᴀ ❈", callback_data="helper")]]
             ),
         )
     )
